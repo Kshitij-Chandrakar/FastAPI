89888354d13bb1370318f4469d0d6dbe82724574
Kshitij-Chandrakar <chandrakarkshitij2208@gmail.com>
2026-04-11 22:48:57 +0530

    patients view data

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
+import json
 app = FastAPI() 
+
+def load_data():
+    # Simulate loading data from a file or database
+    with open("patients.json", "r") as file:
+        data = json.load(file)
+    return data
 
 
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "Patients Management System"}
 
 @app.get("/about")
 def about():
-    return {"message": "Us"}
-@app.get("/contact")
-def contact():
-    return {"message": "Contact Us"}
+    return {"message": "About Us"}
+@app.get("/view")
+def view():
+    data = load_data()  
+    return data
